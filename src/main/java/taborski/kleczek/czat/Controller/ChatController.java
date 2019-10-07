ff607bd5a388f30b3b8413f66008ffd4f12aa255
Dawid Taborski <dawid.taborski@infosysconsulting.com>
2019-10-07 11:40:35 +0200

    [Controller] dodany controller

---
+++ src/main/java/taborski/kleczek/czat/Controller/ChatController.java
@@ -0,0 +1,7 @@
+package taborski.kleczek.czat.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+}
--- src/main/java/taborski/kleczek/czat/model/Message.java
+++ src/main/java/taborski/kleczek/czat/model/Message.java
@@ -4,4 +4,13 @@ import lombok.Data;
 
 @Data
 public class Message {
+    private Type type;
+    private String content;
+    private String sender;
+
+    public enum Type {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
 }
