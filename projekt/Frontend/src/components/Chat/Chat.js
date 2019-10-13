064bbb30c72cc9b0f3840e08f3dc9f349ee9583a
Dawid Taborski <taborski.dawid@gmail.com>
2019-10-13 18:45:21 +0200

    Podlaczanie websocketu

--- projekt/Frontend/src/components/Chat/Chat.js
+++ projekt/Frontend/src/components/Chat/Chat.js
@@ -4,8 +4,25 @@ import SockJsClient from "react-stomp";
 
 const Chat = () => {
   const [connected, setConnected] = useState(false);
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
+  const onMessageReceive = (msg, topic) => {
+    console.log("TCL: onMessageReceive -> msg", msg);
+    // this.setState(prevState => ({
+    //   messages: [...prevState.messages, msg]
+    // }));
+  };
+
+  const sendMessage = (msg, selfMsg) => {
+    console.log("TCL: sendMessage -> selfMsg", selfMsg);
+    // try {
+    //   this.clientRef.sendMessage("/app/all", JSON.stringify(selfMsg));
+    //   return true;
+    // } catch(e) {
+    //   return false;
+    // }
+  };
   return (
     <div id="chat-page">
       <div className="chat-container">
@@ -15,10 +32,21 @@ const Chat = () => {
         {!connected ? (
           <div className="connecting">Connecting...</div>
         ) : (
-          <ul id="messageArea"></ul>
+          <ul id="messageArea">
+            {messages.map(text => (
+              <li>{text}</li>
+            ))}
+          </ul>
         )}
 
-        <form id="messageForm" name="messageForm">
+        <form
+          id="messageForm"
+          name="messageForm"
+          onSubmit={event => {
+            setMessage("");
+            event.preventDefault();
+          }}
+        >
           <div className="form-group">
             <div className="input-group clearfix">
               <input
@@ -26,6 +54,8 @@ const Chat = () => {
                 id="message"
                 placeholder="Type a message..."
                 autoComplete="off"
+                value={message}
+                onChange={event => setMessage(event.target.value)}
                 className="form-control"
               />
               <button type="submit" className="primary" disabled={!connected}>
