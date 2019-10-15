83f4f7c39e462c3433a8745e9b7e68c61c2b5e1f
Dawid Taborski <dawid.taborski@infosysconsulting.com>
2019-10-15 10:15:41 +0200

    init project dla microservisu users

--- projekt/User/src/main/java/taborski/kleczek/User/UserApplication.java
+++ projekt/User/src/main/java/taborski/kleczek/User/UserApplication.java
@@ -1,13 +1,13 @@
-package taborski.kleczek.chat;
+package taborski.kleczek.User;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatApplication {
+public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatApplication.class, args);
+		SpringApplication.run(UserApplication.class, args);
 	}
 
 }
