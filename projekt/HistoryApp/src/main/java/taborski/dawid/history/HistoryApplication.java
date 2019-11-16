b6c11f4aece44ec27b6f02de59651e96c5ca85ec
Dawid Taborski <taborski.dawid@gmail.com>
2019-11-16 12:16:05 +0100

    init commit

---
+++ projekt/HistoryApp/src/main/java/taborski/dawid/history/HistoryApplication.java
@@ -0,0 +1,13 @@
+package taborski.dawid.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryApplication.class, args);
+	}
+
+}
