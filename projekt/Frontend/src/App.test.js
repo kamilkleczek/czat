c68ac45b6700f5a5634d1e39535927fca23476f7
Dawid Taborski <taborski.dawid@gmail.com>
2019-12-09 11:43:12 +0100

    docker compose dodany

--- projekt/Frontend/src/App.test.js
+++ projekt/Frontend/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import LoginPage from './components/LoginPage/LoginPage';
+import Chat from './components/Chat/Chat';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('invalid path should redirect to 404', () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={[ '/' ]}>
+      <App/>
+    </MemoryRouter>
+  );
+  expect(wrapper.find(LoginPage)).toHaveLength(1);
+  expect(wrapper.find(Chat)).toHaveLength(0);
 });
